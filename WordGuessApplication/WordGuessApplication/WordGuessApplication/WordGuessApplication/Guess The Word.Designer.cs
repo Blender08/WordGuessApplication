8047e9717693d5239263a08e6d647b0b65a49c1c
Blender08 <blenderfreezer@gmail.com>
2024-10-22 02:15:20 +0800

    Add files via upload

---
+++ WordGuessApplication/WordGuessApplication/WordGuessApplication/WordGuessApplication/Guess The Word.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WordGuessApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHint = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.skipBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(363, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(124, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(363, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wrong Guesses!";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtGuess.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtGuess.Location = new System.Drawing.Point(12, 222);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(342, 26);
+            this.txtGuess.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Location = new System.Drawing.Point(66, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.YellowGreen;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(93, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 47);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnHint
+            // 
+            this.btnHint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnHint.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.btnHint.Location = new System.Drawing.Point(232, 277);
+            this.btnHint.Name = "btnHint";
+            this.btnHint.Size = new System.Drawing.Size(76, 28);
+            this.btnHint.TabIndex = 5;
+            this.btnHint.Text = "Hint:";
+            this.btnHint.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHint.UseVisualStyleBackColor = false;
+            this.btnHint.Click += new System.EventHandler(this.btnHint_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(265, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // skipBtn
+            // 
+            this.skipBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.skipBtn.BackColor = System.Drawing.Color.LightCyan;
+            this.skipBtn.Location = new System.Drawing.Point(12, 277);
+            this.skipBtn.Name = "skipBtn";
+            this.skipBtn.Size = new System.Drawing.Size(39, 27);
+            this.skipBtn.TabIndex = 7;
+            this.skipBtn.Text = "Skip";
+            this.skipBtn.UseVisualStyleBackColor = false;
+            this.skipBtn.Click += new System.EventHandler(this.skipBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(499, 317);
+            this.Controls.Add(this.skipBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnHint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Guess The Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHint;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button skipBtn;
+    }
+}
+
---
+++ WordGuessApplication/WordGuessApplication/WordGuessApplication/WordGuessApplication/Guess The Word.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace WordGuessApplication
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        WindowsMediaPlayer soundEffect = new WindowsMediaPlayer();
+
+        ArrayList wrongGuesses = new ArrayList();
+        Random rand = new Random();
+
+        string[] words = { "PROGRAM", "BROWSE", "CELLPHONE", "COMPUTER", "DOWNLOAD", "SYSTEM", "DIGITAL", "DEVICE", "NETWORK", "DESKTOP" };
+        string wordToGuess;
+        char[] maskedWord;
+        int hintCounter = 0;
+        const int maxHints = 5;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            TheWord();
+        }
+
+        private void TheWord()
+        {
+            bgMusic();
+
+            label3.Text = $"{hintCounter}/{maxHints}";
+            wordToGuess = words[rand.Next(0, words.Length)];
+
+            maskedWord = new string('?', wordToGuess.Length).ToCharArray();
+            label2.Text = new string(maskedWord);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string guessWord = txtGuess.Text.ToUpper();
+            StringBuilder message = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(guessWord))
+            {
+                MessageBox.Show("Please enter a guess");
+            }
+            else if (guessWord == wordToGuess)
+            {
+                winnerSE();
+                label2.Text = wordToGuess;
+                message.Append("Correct! The word is " + wordToGuess);
+                MessageBox.Show(message.ToString());
+                disableAllbtns();
+                askToPlayAgain();
+            }
+            else
+            {
+                wrongAnswer();
+                wrongGuesses.Add(guessWord);
+                listBox1.Items.Add(guessWord);
+
+                message.Append("Wrong guess! Try again.");
+                MessageBox.Show(message.ToString());
+            }
+            txtGuess.Clear();
+        }
+
+        private void btnHint_Click(object sender, EventArgs e)
+        {
+            if (hintCounter >= maxHints)
+            {
+                btnHint.Enabled = false;
+                wrongAnswer();
+                MessageBox.Show("You have used all 5 hints.");
+            }
+            else if (hintCounter < maxHints)
+            {
+                hintSE();
+                int randLetter;
+                do
+                {
+                    randLetter = rand.Next(0, wordToGuess.Length);
+                } while (maskedWord[randLetter] != '?');
+
+                maskedWord[randLetter] = wordToGuess[randLetter];
+
+                label2.Text = new string(maskedWord);
+
+                hintCounter++;
+                label3.Text = $"{hintCounter}/{maxHints}";
+            }
+        }
+
+        private void skipBtn_Click(object sender, EventArgs e)
+        {
+            loserSE();
+            label2.Text = wordToGuess;
+            MessageBox.Show("You Skip! \nThe Word is " + wordToGuess);          
+            disableAllbtns();
+            askToPlayAgain();
+        }
+        private void askToPlayAgain()
+        {
+            DialogResult result = MessageBox.Show("Do you want to play again?", "Play Again", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                wrongGuesses.Clear();
+                listBox1.Items.Clear();
+                txtGuess.Clear();
+                hintCounter = 0;
+                disableAllbtns();
+                TheWord();               
+            }
+            else
+            {
+                MessageBox.Show("Thank you for playing! :)");
+                this.Close();
+            }
+        }
+        private void disableAllbtns()
+        {
+            txtGuess.Enabled = true;
+            button1.Enabled = true;
+            btnHint.Enabled = true;
+            skipBtn.Enabled = true;
+        }
+        private void bgMusic()
+        {
+            player.URL = "Windmill Hut - The Legend of Zelda_ Ocarina Of Time.mp3";
+            player.controls.play();
+        }
+        private void winnerSE()
+        {
+            player.URL = "YEHEY CLAP SOUND EFFECT Awarding.mp3";
+        }
+        private void loserSE()
+        {
+            player.URL = "The Price is Right Losing Horn - Sound Effect (HD).mp3";
+        }
+        private void wrongAnswer()
+        {
+            soundEffect.URL = "wrong answer Sound effect No Copyright.mp3";
+            soundEffect.controls.play();
+        }
+        private void hintSE()
+        {
+            soundEffect.URL = "Game Magic Hint Sound Effect  SFX Sound.mp3";
+            soundEffect.controls.play();
+        }
+    }
+}
+
